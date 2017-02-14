c809d48d33abea52436a39d5e4005545ce4d44b0
The-HooK <joel.2.rovera@gmail.com>
2017-02-14 22:09:47 +0100

    Create scandal-wget.js

---
+++ scandal-wget.js
@@ -0,0 +1,5 @@
+var reg = new RegExp('s?([0-9]{1,2})[e,x]?([0-9]{1,2})', 'ig');
+var url = document.getElementsByTagName('video') [0].src;
+var matches = reg.exec(document.getElementById('file_title').innerText);
+var fileName = 'Scandal S0' + matches[1] + '.e' + matches[2] + '.mp4';
+window.prompt('Copy to clipboard: Ctrl+C, Enter', 'wget -O "' + fileName + '" ' + url);
